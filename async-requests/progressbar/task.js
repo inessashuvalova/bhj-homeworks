acf5bc75a0c8bd3ae085b03203db9ed1ab605d63
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2021-03-22 22:18:11 +0300

    changed progressbar/task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,17 +1,24 @@
 'use strict';
 
 const progress = document.getElementById('progress');
+const formElement = document.getElementById('form');
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/upload.php');
-xhr.send();
-xhr.onload = function () {
-    if (xhr.status != 200) {
-        alert('Ошибка: ' + xhr.status);
-        return;
-    }};
-    xhr.onprogress = function(event) { 
-        if (event.lengthComputable) {
-        let number = event.total / event.loaded;
-        console.log(number);
-    }}
+formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    xhr.send();
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+            alert('Ошибка: ' + xhr.status);
+            return;
+        }};
+        xhr.onprogress = function(event) { 
+            if (event.lengthComputable) {
+            let number = event.total / event.loaded;
+            console.log(number)
+        }}
+
+}, false);
+ 
+
